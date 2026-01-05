196b8d847eeba9270614ae34dda5419796ee1bc1
beackdreept <metamorphosiss162@gmail.com>
2026-01-05 06:41:46 -0400

    Fix port binding for Koyeb

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def nemu_brain():
     return "Status: Healthy", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
